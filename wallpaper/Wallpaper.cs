caf838d582b59a51b5ad9ec5db10998b084a1658
小旭喵o <2234975857@qq.com>
2018-01-28 21:11:36 +0800

    fix the file path has space

--- wallpaper/Wallpaper.cs
+++ wallpaper/Wallpaper.cs
@@ -49,7 +49,7 @@ namespace wallpaper
             {
                 timer.Enabled = false;
                 System.Threading.Thread.Sleep(1000);
-                string file = RegistryEdit.GetSetting("videoLocation") + " --hwdec=auto --loop-file=yes";
+                string file = "\"" + RegistryEdit.GetSetting("videoLocation") + "\" --hwdec=auto --loop-file=yes";
                 if (RegistryEdit.GetSetting("mpvAudio") == "False")
                 {
                     file += " --ao=null";
